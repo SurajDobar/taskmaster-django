4a4a65e67b6a1b4277b463209ca6b1b497ff6755
suraj dobar <surajdobar50@gmail.com>
2026-09-05 23:35:23 +0530

    add api user based permissions

--- api/views.py
+++ api/views.py
@@ -1,23 +1,30 @@
+from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+
 from .serializers import ItemSerializers
 from tasks.models import Task
 
+# Create your views here.
+
+#get data in response 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getData(request):
-    tasks = Task.objects.all()
-    if request.query_params.get('completed'):
-        tasks = tasks.filter(completed=True)
+    tasks = Task.objects.filter(user=request.user)
     serializer = ItemSerializers(tasks, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getSingle(request, pk):
-    task = Task.objects.get(id=pk)
+    task = get_object_or_404(Task, id=pk, user=request.user)
     serializer = ItemSerializers(task)
     return Response(serializer.data)
 
+#add data in response
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def addData(request):
@@ -26,18 +33,20 @@ def addData(request):
         serializer.save(user=request.user)
     return Response(serializer.data)
 
+#edit data in response 
 @api_view(['PUT', 'PATCH'])
 @permission_classes([IsAuthenticated])
 def editData(request, pk):
-    task = Task.objects.get(id=pk)
+    task = get_object_or_404(Task, id=pk, user=request.user)
     serializer = ItemSerializers(task, data=request.data, partial=True)
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
 
+#delete data in response 
 @api_view(['DELETE'])
 @permission_classes([IsAuthenticated])
 def deleteData(request, pk):
-    task = Task.objects.get(id=pk)
+    task = get_object_or_404(Task, id=pk, user=request.user)
     task.delete()
     return Response({'message': 'task deleted'})
